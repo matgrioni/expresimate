5348ca3e9e4d1bc08afa488784ddba4204092cc2
Matias Grioni <matgrioni@gmail.com>
2016-12-09 00:28:57 -0500

    Added context of already existing user to NewUser, to avoid duplicates

--- new_user.cpp
+++ new_user.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -5,14 +6,21 @@
 #include "new_user.hpp"
 #include "user.hpp"
 
+NewUser::NewUser(std::vector<User> users) : users_(users)
+{ }
+
 void NewUser::operator() ()
 {
     std::ofstream users("./data/users.dat", std::ios::app);
 
-    std::string name;
-    std::cout << "Player name > ";
     std::cin.ignore(100, '\n');
-    getline(std::cin, name);
+    std::string name;
+    do
+    {
+        std::cout << "Player name > ";
+        getline(std::cin, name);
+    }
+    while (user_name_exists(name));
 
     int age;
     std::cout << "Age > ";
@@ -22,4 +30,12 @@ void NewUser::operator() ()
     users << u.pickle() << std::endl;
 
     users.close();
+}
+
+bool NewUser::user_name_exists(std::string name)
+{
+    auto it = std::find_if(users_.begin(),
+                           users_.end(),
+                           [name] (User u) { return u.name() == name; });
+    return it != users_.end();
 }
--- new_user.hpp
+++ new_user.hpp
@@ -2,11 +2,20 @@
 #define NEW_PLAYER_CALLBACK_HPP
 
 #include <string>
+#include <vector>
+
+#include "user.hpp"
 
 class NewUser
 {
     public:
+        NewUser(std::vector<User> users);
         void operator() ();
+
+    private:
+        bool user_name_exists(std::string name);
+
+        std::vector<User> users_;
 };
 
 #endif
