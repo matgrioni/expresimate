3d52091b0fa0cbe4ec87bccb99926b2f556f67cf
Matias Grioni <matgrioni@gmail.com>
2016-12-09 00:29:29 -0500

    A basic menu layout with ability to add users

--- main.cpp
+++ main.cpp
@@ -1,16 +1,46 @@
+#include <fstream>
 #include <iostream>
-#include "tree.h"
+#include <vector>
+
+#include "expression_factory.hpp"
+#include "start.hpp"
+#include "new_user.hpp"
+#include "menu.hpp"
+#include "user.hpp"
+
+/* Loads users stored in data/users.dat into vector reference
+ * provided. */
+void load_users(std::vector<User> &users);
 
 int main()
 {
-    BinaryTree<int> right_tree;
-    right_tree.node(2);
-    BinaryTree<int> left_tree;
-    left_tree.node(3);
-    BinaryTree<int> tree(1, &left_tree, &right_tree);
-    tree.node(1);
+    std::vector<User> users;
+    load_users(users);
 
-    std::cout << tree.node() << tree.right().node() << tree.left().node() ;
+    ExpressionFactory factory;
+    Expression e = factory.create(2, 0, 10);
+
+    Menu m;
+    m.add_entry("Start", Start());
+    m.add_entry("New user", NewUser(users));
+
+    m.run();
 
     return 0;
+}
+
+void load_users(std::vector<User> &users)
+{
+    std::ifstream users_file("./data/users.dat");
+
+    std::string pickle;
+    User u;
+
+    while (getline(users_file, pickle))
+    {
+        u.depickle(pickle);
+        users.push_back(u);
+    }
+
+    users_file.close();
 }
