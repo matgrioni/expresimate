25344398ddfcd2b11b07cd78b95db815a3cd1149
Matias Grioni <matgrioni@gmail.com>
2016-12-11 13:17:16 -0500

    Ability to save users into store and update existing entries

--- util.cpp
+++ util.cpp
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include "util.hpp"
@@ -27,6 +29,48 @@ namespace util
         {
             u.depickle(pickle);
             users.push_back(u);
+        }
+
+        users_file.close();
+    }
+
+    void save_users(std::vector<User> users)
+    {
+        std::vector<User> saved_users;
+        load_users(saved_users);
+
+        std::ofstream users_file("./data/users.dat");
+
+        for (std::vector<User>::iterator it = saved_users.begin();
+             it != saved_users.end();
+             it++)
+        {
+            // If the current user is to be updated since it was in
+            // the provided list, then copy it over.
+            std::string name = it->name();
+            auto result = std::find_if(users.begin(), users.end(),
+                                      [name] (User u) { return name == u.name(); });
+            if (result != users.end())
+            {
+                users_file << result->pickle();
+                users.erase(result);
+            }
+            else
+            {
+                users_file << it->pickle();
+            }
+
+            users_file << std::endl;
+        }
+
+        // For any remaining users that are new and not updating, save
+        // them in the file.
+        for (std::vector<User>::iterator it = users.begin();
+             it != users.end();
+             it++)
+        {
+            users_file << it->pickle();
+            users_file << std::endl;
         }
 
         users_file.close();
--- util.hpp
+++ util.hpp
@@ -12,6 +12,11 @@ namespace util
     /* Loads users stored in data/users.dat into vector reference
      * provided. */
     void load_users(std::vector<User> &users);
+
+    /* This saves the given users into the data/users.dat store. If
+     * the user already exists, the record is updated. Otherwise,
+     * a new entry is made. */
+    void save_users(std::vector<User> users);
 }
 
 #endif
